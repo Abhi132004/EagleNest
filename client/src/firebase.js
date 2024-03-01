35193e7b253b55c770ba16a5c4cc00467a86db95
Abhi132004 <abhishek.pokale004@gmail.com>
2024-03-01 16:02:06 +0530

    change

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "eagle-nest-c385e.firebaseapp.com",
+  projectId: "eagle-nest-c385e",
+  storageBucket: "eagle-nest-c385e.appspot.com",
+  messagingSenderId: "272430692389",
+  appId: "1:272430692389:web:a217843df98b15f1f227cc"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
